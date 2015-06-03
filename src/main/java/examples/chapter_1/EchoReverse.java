13bfdd7430af2368ae250124fc5e47de75ae200e
Almaz <muktaalmaz@yandex.ru>
2015-06-03 18:59:12 +0300

    Added echo example

---
+++ src/main/java/examples/chapter_1/EchoReverse.java
@@ -0,0 +1,13 @@
+package examples.chapter_1;
+
+/**
+ * Created by almu0214 on 03.06.2015.
+ */
+public class EchoReverse {
+    public static void reverse(String s){
+        if(s == null)
+            throw new NullPointerException("Oooooops!!! This is NPE Exception");
+        System.out.print(new StringBuilder(s).reverse().toString());
+        return;
+    }
+}
